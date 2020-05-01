2dd849f8f750fd6c4313faa3ca0495324a7ddec2
alaa m ebrahim <alaamebrahim91@gmail.com>
2020-05-01 15:45:05 -0400

    remove group by

--- app/http/controllers/PlacesController.php
+++ app/http/controllers/PlacesController.php
@@ -52,7 +52,6 @@ class PlacesController extends Controller
 
 
          $places = Place::select('*')
-                        ->groupBy(['places.id', 'places.place_name'])
                         ->orderBy('places.id','desc')
                         ->get();
          return view('place.index', compact('places'));
