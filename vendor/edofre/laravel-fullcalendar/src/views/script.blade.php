b186ca3faea7752050a53eca44e129323fe638f7
Muhammad Essam <mohammedessam699@gmail.com>
2020-05-26 12:24:10 +0200

    removes vendor

--- vendor/edofre/laravel-fullcalendar/src/views/script.blade.php
+++
@@ -1,9 +0,0 @@
-@if($include_scripts)
-    @include('fullcalendar::files')
-@endif
-
-<script type="text/javascript">
-    jQuery(document).ready(function () {
-        jQuery('#{{ $id }}').fullCalendar({!! $options !!});
-    });
-</script>
